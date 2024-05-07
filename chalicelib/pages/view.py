a6d4d0f14b2790db6ba3edac4bb075f1a61d304b
Aleck <me@myners.net>
2024-05-07 13:52:03 +0000

    style update

--- chalicelib/pages/view.py
+++ chalicelib/pages/view.py
@@ -2,7 +2,7 @@ import os
 import jinja2
 from chalice.app import Response
 from chalicelib import file
-from chalicelib.utils import build_api_endpoint
+# from chalicelib.utils import build_api_endpoint
 from . import bp, logger
 
 
@@ -14,33 +14,33 @@ def render(templ_path, context):
 @bp.route('/', methods=['GET'])
 def index():
     """Start homepage"""
-   
+
     # url links
     links = {
-        'link1' : {'name' : 'Email', 'url' : 'https://mail.myners.net'},
-        'link2' : {'name' : 'Calendar', 'url' : 'https://calendar.myners.net'},
-        'link3' : {'name' : 'Drive', 'url' : 'https://drive.myners.net'},
-        'link4' : {'name' : "Myners' Days", 'url' : 'http://days.myners.net'},
-        'link5' : {'name' : 'Tech Note', 'url' : 'http://t.creast.win'}
+        'link1': {'name': 'Email', 'url': 'https://mail.myners.net'},
+        'link2': {'name': 'Calendar', 'url': 'https://calendar.myners.net'},
+        'link3': {'name': 'Drive', 'url': 'https://drive.myners.net'},
+        'link4': {'name': "Myners' Days", 'url': 'http://days.myners.net'},
+        'link5': {'name': 'Tech Note', 'url': 'http://t.creast.win'}
     }
 
     # send image base64Url to front-end
     context = {
-        'links' : links,
-        'icon1' : file.get_img_base64Url('icon1.png'),
-        'icon2' : file.get_img_base64Url('icon2.png'),
-        'icon3' : file.get_img_base64Url('icon3.png'),
-        'icon4' : file.get_img_base64Url('icon4.png'),
-        'icon5' : file.get_img_base64Url('icon5.png'),
-        'icon_left' : file.get_img_base64Url('icon_left'),
-        'icon_left1' : file.get_img_base64Url('icon_left1'),
-        'icon_right' : file.get_img_base64Url('icon_right'),
-        'icon_right1' : file.get_img_base64Url('icon_right1'),
+        'links': links,
+        'icon1': file.get_img_base64Url('icon1.png'),
+        'icon2': file.get_img_base64Url('icon2.png'),
+        'icon3': file.get_img_base64Url('icon3.png'),
+        'icon4': file.get_img_base64Url('icon4.png'),
+        'icon5': file.get_img_base64Url('icon5.png'),
+        'icon_left': file.get_img_base64Url('icon_left'),
+        'icon_left1': file.get_img_base64Url('icon_left1'),
+        'icon_right': file.get_img_base64Url('icon_right'),
+        'icon_right1': file.get_img_base64Url('icon_right1'),
     }
 
     return Response(
-        body = render('chalicelib/pages/index.html', context),
-        status_code = 200, 
+        body=render('chalicelib/pages/index.html', context),
+        status_code=200,
         headers={"Content-Type": "text/html"}
     )
 
@@ -53,7 +53,7 @@ def get_static_css(file_name):
     try:
         content = file.get_static_file(file_name=css_file)
         return Response(
-            body=content, 
+            body=content,
             status_code=200,
             headers={"Content-Type": "text/css"},
         )
@@ -73,7 +73,7 @@ def get_static_js(file_name):
     try:
         content = file.get_static_file(file_name=js_file)
         return Response(
-            body=content, 
+            body=content,
             status_code=200,
             headers={"Content-Type": "application/javascript; charset=utf-8"},
         )
@@ -83,6 +83,7 @@ def get_static_js(file_name):
             status_code=404,
             headers={"Content-Type": "text/html"},
         )
+
 
 @bp.route("/icon/{file_name}", methods=["GET"])
 def get_icons(file_name):
@@ -96,7 +97,7 @@ def get_icons(file_name):
         img = file.get_static_media(file_name=file_name)
         return Response(
             # return the image in Base64 encoding
-            body=img, 
+            body=img,
             status_code=200,
             headers={"Content-Type": "image/png"}
         )
@@ -107,12 +108,14 @@ def get_icons(file_name):
             headers={"Content-Type": "text/html"},
         )
 
+
 @bp.route("/favicon.ico", methods=["GET"])
 def get_favicon():
     """Get favicon image"""
-    file_type='image/vnd.microsoft.icon'
+    file_type = 'image/vnd.microsoft.icon'
     try:
-        imgurl = file.get_img_base64Url(file_name='favicon.ico', file_type=file_type)
+        imgurl = file.get_img_base64Url(
+            file_name='favicon.ico', file_type=file_type)
         icon = f'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32"><image width="32" height="32" xlink:href="{imgurl}"/></svg>'
         return Response(
             body=icon,
